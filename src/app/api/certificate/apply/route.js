f8f0ed2f4b8d7bc0b7f1a6f129e3d4060d3c37a8
Gaurav Khandelwal <gauravkhandelwal0309@gmail.com>
2026-01-02 12:20:48 +0530

    certificate added

--- src/app/api/certificate/apply/route.js
+++ src/app/api/certificate/apply/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import connectDB from "@/lib/db";
-import CertificateRequest from "@/models/CertificateRequest";
+import CertificateRequest from "@/models/certificateRequest";
 
 // POST: submit certificate request
 export async function POST(req) {
